4959f1b9696a7f9c53adb5bf85bb6509bfd33f63
Michael Burkhardt <michael@peroxaan.com>
2022-12-11 18:38:29 -0500

    landon not that guy tbh

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,6 @@ export const ACCEPTED_PNG =
 export const DENIED_PNG =
 	"https://media.discordapp.net/attachments/809297772850839552/1051365281294991370/Denied.png";
 
-export const OWNERS_IDS = ["275775739998240769", "622228854064414721"];
+export const OWNERS_IDS = ["275775739998240769"];
 
 export const PEROXAAN_COLOR = `#FE6C8B`;
